faf547154dbae49960592d92c9001168558482f1
Sebayan Bhattacharjee <Sebayanbhattachergee@gmail.com>
2024-09-22 11:52:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+const darkMode = document.querySelector('.dark-mode');
+const menuLinks = document.querySelectorAll('.menu-link');
+
+
+
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+});
+
+darkMode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode-variables');
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
+  // Add a click event listener to each link
+  menuLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault(); // Prevents the link from navigating
+
+      // Remove the "active" class from all links
+      menuLinks.forEach(item => {
+        item.classList.remove('active1');
+      });
+
+      // Add the "active" class to the clicked link
+      link.classList.add('active1');
+    });
+  });
+
+// Orders 
+
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+        <td>${order.productName}</td>
+        <td>${order.productNumber}</td>
+        <td>${order.paymentStatus}</td>
+        <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning' : 'primary'}">${order.status}</td>
+        <td class="primary">Details</td>
+    `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+});
